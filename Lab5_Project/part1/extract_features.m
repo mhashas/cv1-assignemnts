2a02778e07e208f6dd3a8d5a6fb5d363680500d5
Radu Sibechi <radu@digifit.eu>
2018-03-21 16:49:21 +0100

    replaced vlphow with vldense, debugging complete, features returned seem to be good

--- Lab5_Project/part1/extract_features.m
+++ Lab5_Project/part1/extract_features.m
@@ -3,16 +3,14 @@ function [keypoints, descriptors] = extract_features(dataset, colorspace, dense)
 descriptors = [];
 keypoints = [];
 
-for i=1:size(dataset)
-    image = dataset{i};
+for i=1:size(dataset, 2)
+    image = im2single(dataset{i});
     
     switch colorspace
         case 'gray'
             if size(image, 3) > 1
-                image = rg2bgray(image);
+                image = rgb2gray(image);
             end
-            
-            image = single(image);
             
             if dense
                 [k, d] = vl_dsift(image, 'step', 10);
@@ -21,45 +19,43 @@ for i=1:size(dataset)
             end
             
             keypoints = [keypoints, k];
-            descriptors = [descriptors, double(d];
-                
+            descriptors = [descriptors, double(d)];
+            
         case 'RGB'
-        if size(image, 3) == 3
-            image = single(image);
-            k = [];
-            d = [];
-            if dense
-                [k, d] = vl_phow(image, 'Color', 'rgb', 'step', 10);
-                d = double(d);
-            else
+            if size(image, 3) == 3
+                k = [];
+                d = [];
+                
                 for c=1:size(image, 3)
-                    [c_k, c_d] = vl_sift(image(:,:,c));
+                    if dense
+                        [c_k, c_d] = vl_dsift(image(:,:,c), 'step', 10);
+                    else
+                        [c_k, c_d] = vl_sift(image(:,:,c));
+                    end
                     d = [d, double(c_d)];
                     k = [k, c_k];
                 end
+                
+                keypoints = [keypoints, k];
+                descriptors = [descriptors, d];
+            else
+                warning('RGB called for grayscale');
             end
             
-            keypoints = [keypoints, k];
-            descriptors = [descriptors, d];
-        else
-            warning('RGB called for grayscale');
-        end
-        
         case 'rgb'
             if size(image, 3) == 3
                 k = [];
                 d = [];
-                image = single(rgb2norm(image));
+                image = im2single(rgb2norm(image));
                 
-                if dense
-                    [k, d] = vl_phow(image, 'Color', 'rgb', 'step', 10);
-                    d = double(d);
-                else
-                    for c=1:size(image, 3)
+                for c=1:size(image, 3)
+                    if dense
+                        [c_k, c_d] = vl_dsift(image(:,:,c), 'step', 10);
+                    else
                         [c_k, c_d] = vl_sift(image(:,:,c));
-                        d = [d, double(c_d)];
-                        k = [k, c_k];
                     end
+                    d = [d, double(c_d)];
+                    k = [k, c_k];
                 end
                 
                 keypoints = [keypoints, k];
@@ -71,21 +67,28 @@ for i=1:size(dataset)
         case 'opponent'
             k = [];
             d = [];
-            image = single(rgb2opponent(image));
+            image = im2single(rgb2opponent(image));
+            
             if size(image,3) == 3
-                if dense
-                    [k, d] = vl_phow(image, 'color' , 'opponent', 'step', '10');
-                else
-                    for c=1:size(image,3)
+                
+                for c=1:size(image,3)
+                    if dense
+                        [c_k, c_d] = vl_dsift(image(:,:,c), 'step', 10);
+                    else
                         [c_k, c_d] = vl_sift(image(:,:,c));
-                        d = [d, double(c_d)];
-                        k = [k_, c_k];
                     end
+                    d = [d, double(c_d)];
+                    k = [k, c_k];
                 end
                 
                 keypoints = [keypoints, k];
                 descriptors = [descriptors, d];
+            else
+                warning('rgb called for grayscale'); 
             end
     end
 end
+
+descriptors = descriptors';
+
 end
--- Lab5_Project/part1/main.m
+++ Lab5_Project/part1/main.m
@@ -3,10 +3,13 @@ clc
 close all
 
 %user defined variables
-number_of_images = 50;
-dense = true; % true if we should extract SIFT descriptors densely
-colorspace = 'rgb' % possible values are: gray, rgb, RGB, opponent 
+number_of_images = 2;
+dense = false; % true if we should extract SIFT descriptors densely
+colorspace = 'opponent'; % possible values are: gray, rgb, RGB, opponent 
 
-dataset = load_dataset(number_of_images);
+complete_dataset = load_dataset(number_of_images);
 
-for i
+[~, features1] = extract_features(complete_dataset{1}, colorspace, dense); 
+
+disp(isequal(features1, features2));
+
