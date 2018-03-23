c30a02e0465bc21a1fe6d108795b40d6a9853e00
NichitaDiaconu <diacon995@gmail.com>
2018-03-23 16:03:24 +0100

    feature histogram and SVM. SVM NOT TESTED

---
+++ Lab5_Project/part1/image_feature_histogram.m
@@ -0,0 +1,16 @@
+function histogram = image_feature_histogram(centers, image, colorspace, dense)
+% centers: 2D array cluster centers (vocabulary words centers)
+% centers: 128 X number of centers
+
+histogram = zeros(size(centers,2),1);
+%% get all the sift features
+[~, d] = get_image_features(image, colorspace, dense);
+
+%% assign each feature to a cluster and count
+for feature_idx = 1,size(d,2)
+    feature = d(:, feature_idx);
+    [~, k] = min(vl_alldist(feature, centers));
+    histogram(k) = histogram(k) + 1;
+end
+%normalize histogram
+histogram = histogram / sum(histogram); 
