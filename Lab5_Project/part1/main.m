8f6222ae4b421a09fa25122fb3f75d8894532052
NichitaDiaconu <diacon995@gmail.com>
2018-03-23 15:10:32 +0100

    clustering and main outline

--- Lab5_Project/part1/main.m
+++ Lab5_Project/part1/main.m
@@ -2,6 +2,7 @@ clear
 clc
 close all
 
+%% LOAD DATA
 %user defined variables
 number_of_images = 2;
 dense = 1; % true if we should extract SIFT descriptors densely
@@ -9,6 +10,7 @@ colorspace = 'opponent'; % possible values are: gray, rgb, RGB, opponent
 
 complete_dataset = load_dataset(number_of_images);
 
+%% COLLECT SIFT FEATURES
 sample_random_image_features = 1;
 sample_size = 200;
 descriptors = [];
@@ -16,3 +18,7 @@ for class = 1:4
     [~, class_descriptors] = extract_features(complete_dataset{class}, colorspace, dense, sample_random_image_features, sample_size);
     descriptors = [descriptors, class_descriptors];
 end
+
+%% CLUSTER FEATURES
+vocab_size = 1000; 
+[centers, assignments] = vl_kmeans(descriptors, vocab_size, 'Initialization', 'plusplus', 'Algorithm' ,'Lloyd');
