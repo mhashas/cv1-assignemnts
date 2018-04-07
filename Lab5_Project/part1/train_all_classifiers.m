3837fed7b5dc01b66dd8c490efcbbe2a7d9b851a
Radu Sibechi <radu@digifit.eu>
2018-04-07 13:35:56 +0200

    training classifier from saved vocab

---
+++ Lab5_Project/part1/train_all_classifiers.m
@@ -0,0 +1,26 @@
+sift_types = [0]; % 0 or 1 for dense or normal sift
+color_spaces = ["RGB", "rgb", "gray", "opponent"];
+% if set to 1, we extract keypoints from gray image and the descriptors of those keypoint from image transformed into colorspace. 
+% else we extract descriptors directly from image transformed into colorspace
+extract_from_gray = 0;
+vocabulary_sizes = [400, 800, 1600, 2000, 4000]; % k in kmeans
+vocabulary_images = 200; % number of images used for training the vocabulary
+max_features = 50 * vocabulary_images * 4; % max number of features 
+max_iter = 500; % max number of iterations for kmeans
+test_images_per_class = 50;
+
+
+k = 400;
+dense_string = 'normal';
+color = "RGB";
+train_images_per_class = 50;
+
+vocab_location = sprintf('saved_vocabs/%d_images_%d_vocabsize_%s_%s.mat', vocabulary_images, k, dense_string, color);
+load(vocab_location, 'centers');
+
+if ~centers
+    disp('YOOOOO');
+else
+    [classifiers, test_set, test_features] = train_classifier(vocabulary_images, train_images_per_class, test_images_per_class, 0, color, k, max_features, centers);
+    mAP = evaluate(classifiers, test_set, test_features, test_images_per_class);
+end
--- Lab5_Project/part1/train_classifier.m
+++ Lab5_Project/part1/train_classifier.m
@@ -1,12 +1,18 @@
-function [classifiers, test_set, test_features] =  train_classifier(vocabulary_images, train_images_per_class, test_images_per_class, dense, colorspace, vocabulary_size, max_features)
+function [classifiers, test_set, test_features] =  train_classifier(vocabulary_images, train_images_per_class, test_images_per_class, dense, colorspace, vocabulary_size, max_features, centers)
+
+if nargin < 8
+    centers = 0;
+end
 
 [train_vocab_set, train_set, test_set] = load_dataset(vocabulary_images, train_images_per_class);
 
-%% COLLECT SIFT FEATURES
-descriptors = extract_sift_features(train_vocab_set, colorspace, dense, max_features);
+if ~centers
+    %% COLLECT SIFT FEATURES
+    descriptors = extract_sift_features(train_vocab_set, colorspace, dense, max_features);
 
-%% BUILD VISUAL VOCAB
-[~, centers] = build_visual_vocab(descriptors, vocabulary_size);
+    %% BUILD VISUAL VOCAB
+    [~, centers] = build_visual_vocab(descriptors, vocabulary_size);
+end
 
 %% BUILD FEATURES
 train_features = build_features(train_set, centers, colorspace, dense, train_images_per_class);
@@ -20,8 +26,8 @@ for i = 1:4
     labels((i-1)*train_images_per_class+1: (i)*train_images_per_class) = ones(1, train_images_per_class);
     labels = double(labels);
     
-    classifiers{i} = train(labels', sparse(train_features), '-s 1');
-   
+    best = train(labels', sparse(train_features), '-C -s 0');
+    classifiers{i} = train(labels', sparse(train_features), sprintf('-c %f -s 0', best(1) + 0.00001));   
 end
 
 end
